04c779e79244bd336c0b5be0c59e083894d211f6
Jose L Collins <Jose L Collins>
2016-09-28 20:23:48 -0500

    Adding Angular files

--- NPCMSApp/src/NPCMSApp/wwwroot/_references.js
+++ NPCMSApp/src/NPCMSApp/wwwroot/_references.js
@@ -6,6 +6,7 @@
 /// <reference path="lib/angular-resource/angular-resource.js" />
 /// <reference path="lib/angular-touch/angular-touch.js" />
 /// <reference path="lib/angular-ui-router/release/angular-ui-router.js" />
+/// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
